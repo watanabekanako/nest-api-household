36a455f6e4f5e60d8ebb8d84d0289f8bc6a59ab8
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-16 19:21:11 +0900

    post Get findAllメソッド完了

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 @Controller('post')
-export class PostController {}
+export class PostController {
+  // http://localhost:3005/post にてreturnのなかが返却される
+  @Get()
+  async findMany() {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        category: true,
+
+        tags: {
+          include: {
+            tag: true,
+          },
+        },
+      },
+    });
+    return { posts };
+  }
+}
