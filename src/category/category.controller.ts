7910c0266a392dcfb12427013d7ed8cbe4499579
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-17 09:28:03 +0900

    category　controller Getメソッド完了

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  @Get()
+  async findMany() {
+    const categories = await prisma.category.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+      // includeはrelationを取得
+      include: {
+        _count: {
+          // _countは紐づく投稿の数
+          select: {
+            // postsはCategoryテーブルのposts
+            posts: true,
+          },
+        },
+      },
+    });
+    return { categories };
+  }
+}
