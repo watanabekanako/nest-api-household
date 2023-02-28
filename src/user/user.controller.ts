d54b3a47effec5cc96f5f5dcfe57235576a7b0e3
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-28 10:33:27 +0900

    logon/logout api 実装完了

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,6 +15,7 @@ import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { Request } from 'express';
+
 @UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
