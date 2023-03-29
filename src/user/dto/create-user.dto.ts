9adde5d59a791249db6ef92cfe8a0b751cfffac3
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2023-03-29 17:11:51 +0900

    ユーザー情報更新処理の修正

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -11,3 +11,12 @@ export class CreateUserDto {
   @Length(8, 10)
   password: string;
 }
+
+export class UpdateUserDto {
+  @IsString()
+  @Contains('@')
+  email: string;
+
+  @IsString()
+  password: string;
+}
